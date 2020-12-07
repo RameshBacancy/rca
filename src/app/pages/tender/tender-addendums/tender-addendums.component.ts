0b757be0f5b4a17b07b28565cea01b5902674356
ramesh <ramesh.prajapati@bacancytechnology.com>
2020-12-07 14:57:12 +0530

    tender addendums

--- src/app/pages/tender/tender-addendums/tender-addendums.component.ts
+++ src/app/pages/tender/tender-addendums/tender-addendums.component.ts
@@ -1,3 +1,4 @@
+import { FormControl } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tender-addendums.component.scss']
 })
 export class TenderAddendumsComponent implements OnInit {
-
+  selected = new FormControl(0);
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  changeTab() {
+    this.selected.setValue(this.selected.value + 1);
+  }
+  previousTab() {
+    this.selected.setValue(this.selected.value - 1);
   }
 
 }
