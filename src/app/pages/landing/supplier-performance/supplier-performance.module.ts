c2328060a92305c8af911ca19c27a4d92f9ce98e
ramesh <ramesh.prajapati@bacancytechnology.com>
2020-12-10 19:30:51 +0530

    make supplier-performance stepper

--- src/app/pages/landing/supplier-performance/supplier-performance.module.ts
+++ src/app/pages/landing/supplier-performance/supplier-performance.module.ts
@@ -3,13 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { SupplierPerformanceRoutingModule } from './supplier-performance-routing.module';
 import { SupplierPerformanceComponent } from './supplier-performance.component';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatTabsModule } from '@angular/material/tabs';
 
 
 @NgModule({
   declarations: [SupplierPerformanceComponent],
   imports: [
     CommonModule,
-    SupplierPerformanceRoutingModule
+    SupplierPerformanceRoutingModule,
+    MatStepperModule,
+    MatTabsModule,
   ]
 })
 export class SupplierPerformanceModule { }
