47dab690620538b983e4351bd64b93b0a4a9548f
ramesh <ramesh.prajapati@bacancytechnology.com>
2020-12-31 14:47:08 +0530

    dashboard tendering

--- src/app/pages/landing/dashboard/dashboard.component.ts
+++ src/app/pages/landing/dashboard/dashboard.component.ts
@@ -4,6 +4,7 @@ import { Router } from '@angular/router';
 import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { AlertMessageService } from 'src/app/services/alert-message.service';
 import { SafeHtmlPipe } from 'src/app/pipe/safeHtml.pipe';
+import { AlertService } from 'src/app/services/alert.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -33,6 +34,7 @@ export class DashboardComponent implements OnInit, AfterViewChecked {
     private alertMessage: AlertMessageService,
     private safeHtml: SafeHtmlPipe,
     private tenderService: TenderService,
+    private alerts: AlertService,
     private cdr: ChangeDetectorRef
   ) { }
 
@@ -133,8 +135,50 @@ export class DashboardComponent implements OnInit, AfterViewChecked {
     }
   }
 
-  onTenderStatus() {
-
+  onTenderStatus(status?) {
+    if (localStorage.getItem('RegStatus') === 'finish') {
+      if (localStorage.getItem('arStatus') === 'pending') {
+        this.alertMessage.getMessages().subscribe(d => {
+          d.data.data.filter(a => {
+            if (a.status == 'pending') {
+              this.alertData = a;
+            }
+          });
+          if (this.alertData) {
+            this.status = this.alertData.title;
+            this.approveRejectStatus = this.safeHtml.transform(this.alertData.description, true);
+          } else {
+            this.status = 'Approval Awaited';
+            this.approveRejectStatus = 'Thank you for entering your registration details. <br/> We will review and get back to you soon.<br/>';
+          }
+        });
+        this.gotopath = '/landing/supplier-registration/dashboard';
+      } else if (localStorage.getItem('arStatus') === 'approved') {
+        if(this.tendering[status]){
+          this.gotopath = '/e-tendering/tender-dashboard/current-tenders?status='+status;
+        } else {
+          this.alerts.pushError('No tender available.')
+        }
+      } else if (localStorage.getItem('arStatus') === 'reject') {
+        this.alertMessage.getMessages().subscribe(d => {
+          d.data.data.filter(a => {
+            if (a.status == 'reject') {
+              this.alertData = a;
+            }
+          });
+          if (this.alertData) {
+            this.status = this.alertData.title;
+            this.approveRejectStatus = this.safeHtml.transform(this.alertData.description, true);
+          } else {
+            this.status = 'Rejected';
+            this.approveRejectStatus = 'Your Registration request is Rejected by the Admin. <br/> If you have any queries regarding to this then contact with admin. <br/>';
+          }
+        });
+        this.gotopath = '/landing/supplier-registration/dashboard';
+      }
+    }else {
+      this.router.navigateByUrl('/landing/supplier-registration/registration');
+    }
   }
   goto(cancel?) {
     localStorage.setItem('ModelShowed', 'true');
