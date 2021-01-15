660590fa30e143fa028734802f7d2dca1dad0acf
ramesh <ramesh.prajapati@bacancytechnology.com>
2021-01-15 12:05:37 +0530

    change

--- src/app/pages/landing/supplier-collaboration/components/profile-update/profile-update.component.ts
+++ src/app/pages/landing/supplier-collaboration/components/profile-update/profile-update.component.ts
@@ -1,3 +1,4 @@
+import { FormGroup, FormBuilder, AbstractControl } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileUpdateComponent implements OnInit {
 
-  constructor() { }
+  profileUpdateForm: FormGroup;
+  isProfileUpdate = false;
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+    this.loadForm();
   }
 
+  // load profile form
+  private loadForm(): void {
+    this.profileUpdateForm = this.formBuilder.group({
+      bankAccount1: ['ac1236632112'],
+      bankAccount2: ['ac9874569874'],
+      projects: ['Cms'],
+      address1: ['lorem lorem'],
+      address2: ['lorem lorem 2'],
+      address3: ['lorem lorem 3'],
+      email: ['bala.r@gmail.com'],
+      phone: [9999999999],
+      mobile: [8888888888],
+      fax: ['.'],
+      keyContactPerson: ['R.B.']
+    });
+  }
+
+  // get profile form control
+  get profileFormControl(): { [key: string]: AbstractControl; } {
+    return this.profileUpdateForm.controls;
+  }
+
+  //  profile update submit
+  public profileUpdateSubmit(): void {
+    this.isProfileUpdate = false;
+  }
+
+  // profile update click
+  updateClick(tab: string) {
+    if (tab === 'profile') {
+      this.isProfileUpdate = true;
+    }
+  }
 }
