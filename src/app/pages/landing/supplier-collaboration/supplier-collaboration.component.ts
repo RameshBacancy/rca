5194389729ba7a2dd36fb39802e0e8afd03de14d
ramesh <ramesh.prajapati@bacancytechnology.com>
2021-01-15 12:31:23 +0530

    changes

--- src/app/pages/landing/supplier-collaboration/supplier-collaboration.component.ts
+++ src/app/pages/landing/supplier-collaboration/supplier-collaboration.component.ts
@@ -11,49 +11,7 @@ import { AlertService } from 'src/app/services/alert.service';
   styleUrls: ['./supplier-collaboration.component.scss']
 })
 export class SupplierCollaborationComponent implements OnInit {
-  selected = new FormControl(0);
-  isProfileUpdate = false;
-  isActivityUpdate = false;
-  isRenewalUpdate = false;
-  closeResult: any;
-  supplierType: string;
-
-  paymentData = {
-    amount: null,
-    currency: '',
-    token: ''
-  }
-
-
-  isSave = true;
-  profileUpdateForm: FormGroup;
-  activityUpgradeRequestForm: FormGroup;
-  renewalUpgradeRequestFrom: FormGroup;
-  gradeSelectOption = [
-    { value: 'a', viewValue: 'A' },
-    { value: 'b', viewValue: 'B' },
-    { value: 'b+', viewValue: 'B+' }
-  ];
-  activitySelectOption = [
-    { value: '1', viewValue: 'Activity 1' },
-    { value: '2', viewValue: 'Activity 2' },
-    { value: '3', viewValue: 'Activity 3' }
-  ];
-  activityUpgradeStatus: string;
-  renewalUpgradeStatus: string;
-  activityPaymentStatus: string;
-  renewalPaymentStatus: string;
-
-  @ViewChild('requestModal') requestModal: ElementRef;
-
-  constructor(
-    private formBuilder: FormBuilder,
-    private modalService: NgbModal,
-    private router: Router,
-    private alertMessage: AlertService,
-    private collaborationService: CollaborationService,
-    private cdr: ChangeDetectorRef
-  ) { }
+  constructor( ) { }
 
   ngOnInit(): void {
   }
